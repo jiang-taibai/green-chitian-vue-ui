0587ec4f5286142b5d0d41aefbfedb164702d96b
Jiang Liu <emailtojiang@gmail.com>
2024-09-19 21:01:13 +0800

    使用 IconFont 作为图标

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import './assets/css/style.css'
 import App from './App.vue'
 import router from './assets/js/router'
+import '@/assets/iconfont/iconfont.css'
 
 const app = createApp(App)
 app.use(router)
