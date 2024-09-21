fb71ebf8d5a83351ce4adf02ff103249bc076735
Jiang Liu <emailtojiang@gmail.com>
2024-09-22 01:29:12 +0800

    添加 vue-tmap 依赖以支持 腾讯地图 的显示

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './assets/css/style.css'
 import App from './App.vue'
 import router from './assets/js/router'
 import '@/assets/iconfont/iconfont.css'
+import TMap from '@map-component/vue-tmap';
 
 const app = createApp(App)
 app.use(router)
+app.use(TMap)
 
 app.mount('#app')
