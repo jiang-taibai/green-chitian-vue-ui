ad3ca14c1af8a6d4a75e89b69bbf1a8f78c319ed
Jiang Liu <emailtojiang@gmail.com>
2024-09-22 23:36:38 +0800

    实现部分土壤养分页

--- src/views/user/FarmlandData.vue
+++ src/views/user/FarmlandData.vue
@@ -1,15 +1,30 @@
 <script setup>
-
+import {ref} from "vue";
 import UserTabBar from "@/components/user/UserTabBar.vue";
+import {getLocationText} from "@/assets/js/api/tianditu-api.js";
+import GeoLocationFiled from "@/components/public/GeoLocationFiled.vue";
+
+const location = ref({
+  text: "未知位置",
+  latitude: 18.352992,
+  longitude: 109.155312,
+})
+
 </script>
 
 <template>
   <div>
-    <router-view/>
+    <van-nav-bar title="土壤养分" fixed placeholder/>
+    <div class="container">
+      <geo-location-filed v-model:location="location"/>
+    </div>
     <user-tab-bar active="farmland-data"/>
   </div>
 </template>
 
 <style scoped>
+.container {
 
+
+}
 </style>
