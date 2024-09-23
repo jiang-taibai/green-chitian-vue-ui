186ed65c3c85d1385bc82da09ce249257896b153
Jiang Liu <emailtojiang@gmail.com>
2024-09-23 14:14:07 +0800

    实现土壤养分页面的地理坐标获取与显示部分

--- src/views/user/FarmlandData.vue
+++ src/views/user/FarmlandData.vue
@@ -1,22 +1,31 @@
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import UserTabBar from "@/components/user/UserTabBar.vue";
-import {getLocationText} from "@/assets/js/api/tianditu-api.js";
 import GeoLocationFiled from "@/components/public/GeoLocationFiled.vue";
+import TMap from "@/components/public/TMap.vue";
 
 const location = ref({
   text: "未知位置",
-  latitude: 18.352992,
-  longitude: 109.155312,
+  latitude: 39.924232,
+  longitude: 116.403406,
 })
 
+const coordinate = computed(() => {
+  return {
+    latitude: location.value.latitude,
+    longitude: location.value.longitude,
+  }
+})
 </script>
 
 <template>
   <div>
     <van-nav-bar title="土壤养分" fixed placeholder/>
     <div class="container">
-      <geo-location-filed v-model:location="location"/>
+      <div class="location">
+        <geo-location-filed v-model:location="location"/>
+        <t-map :coordinate="coordinate"/>
+      </div>
     </div>
     <user-tab-bar active="farmland-data"/>
   </div>
@@ -24,7 +33,12 @@ const location = ref({
 
 <style scoped>
 .container {
+  padding: 16px;
+}
 
-
+.location {
+  padding: 16px;
+  border-radius: 8px;
+  background-color: #fff;
 }
 </style>
