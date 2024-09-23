5f92f1c0d3d612fd9f6ac352ad3e367e7cc1a8fb
Jiang Liu <emailtojiang@gmail.com>
2024-09-23 23:02:44 +0800

    稍微修改代码

--- src/views/user/record/Upload.vue
+++ src/views/user/record/Upload.vue
@@ -1,12 +1,12 @@
 <script setup>
-import {computed, defineEmits, ref} from 'vue';
+import {defineEmits, ref} from 'vue';
 import GeoLocationFiled from "@/components/public/GeoLocationFiled.vue";
+import NavBar from "@/components/public/NavBar.vue";
 
 const emits = defineEmits(['commit']);
 
 const afterRead = (file) => {
   console.log(file);
-  //把图片加入
 };
 const detailInfo = ref({
   id: '',
@@ -15,7 +15,8 @@ const detailInfo = ref({
   date: "",
   agroChemicals: "",
   dosageNumber: 0.00,
-  dosageUnit: "g",
+  dosageUnitText: "克",
+  dosageUnitValue: "g",
   note: "",
   location: {
     latitude: 0.00,
@@ -23,9 +24,9 @@ const detailInfo = ref({
   }
 });
 
-const chooseForm = ref({
+const farmlandChooseForm = ref({
   show: false,
-  farmlands: [
+  options: [
     {text: "所有农田", value: "0"},
     {text: "农田1", value: "1"},
     {text: "农田2", value: "2"},
@@ -36,60 +37,75 @@ const chooseForm = ref({
     {text: "农田7", value: "7"},
     {text: "农田8", value: "8"},
     {text: "农田9", value: "9"},
-    {text: "农田10", value: "10"},
+    {text: "农田10", value: "10"}
   ]
 })
-const onFinish = ({selectedOptions}) => {
-  chooseForm.value.show = false;
-  detailInfo.value.farmland = selectedOptions.map((option) => option.text).join('/');
+const dosageUnitChooseForm = ref({
+  show: false,
+  options: [
+    {text: "千克", value: "kg"},
+    {text: "克", value: "g"},
+    {text: "斤", value: "jin"},
+  ]
+})
+
+const onFarmlandChooseFinish = ({selectedOptions}) => {
+  farmlandChooseForm.value.show = false;
+  detailInfo.value.farmland = selectedOptions[0].text;
 };
-const dosageUnit = computed(() => detailInfo.value.dosageUnit + 'g')
-const back = () => history.back();
+const onDosageUnitChooseFinish = ({selectedOptions}) => {
+  dosageUnitChooseForm.value.show = false;
+  detailInfo.value.dosageUnitText = selectedOptions[0].text;
+  detailInfo.value.dosageUnitValue = selectedOptions[0].value;
+};
+
 const commit = (value) => {
-  emits('commit', ...value)
+  emits('commit', {...value})
 }
 </script>
 
 <template>
-  <van-nav-bar title="记录 - 上传" left-text="返回" left-arrow @click-left="back" fixed placeholder/>
+  <nav-bar title="记录 - 上传" back/>
   <div class="container">
     <div class="form">
       <div class="detail">
         <van-cell-group style="margin: 0;">
-          <van-field
-              v-model="detailInfo.farmland"
-              is-link
-              readonly
-              label="农田"
-              placeholder="请选择农田"
-              @click="chooseForm.show = true"
-              required
-          />
-          <van-popup v-model:show="chooseForm.show" round position="bottom">
-            <van-cascader
-                title="请选择农田"
-                :options="chooseForm.farmlands"
-                @close="chooseForm.show = false"
-                @finish="onFinish"
-            />
-          </van-popup>
-          <van-field v-model="detailInfo.agroChemicals" label="农药/化肥" required placeholder="请输入农药/化肥"/>
-          <van-field v-model="detailInfo.dosageUnit" type="digit" label="用量" required
-                     placeholder="请输入用量，单位：千克"/>
-          <van-field v-model="detailInfo.note" label="备注" placeholder="请输入备注" rows="1" autosize type="textarea"/>
+          <van-field v-model="detailInfo.farmland" is-link readonly required
+                     label="农田" placeholder="请选择农田"
+                     @click="farmlandChooseForm.show = true"/>
+          <van-field v-model="detailInfo.agroChemicals" required
+                     label="农药/化肥" placeholder="请输入农药/化肥"/>
+          <van-field v-model="detailInfo.dosageNumber" required
+                     type="number" label="用量" placeholder="请输入用量"/>
+          <van-field v-model="detailInfo.dosageUnitText" is-link readonly required
+                     label="用量单位" placeholder="请选择用量单位"
+                     @click="dosageUnitChooseForm.show = true"/>
+          <van-field v-model="detailInfo.note" autosize
+                     label="备注" placeholder="请输入备注" rows="1" type="textarea"/>
         </van-cell-group>
       </div>
       <div class="position">
         <geo-location-filed :location="detailInfo.location"/>
       </div>
     </div>
-    <div class="uploadImage">
-      <div class="span">上传图片:</div>
-      <van-uploader v-model="detailInfo.fileList" multiple :after-read="afterRead"/>
-    </div>
+    <van-cell-group title="图片上传" style="background: none;">
+      <div class="upload-container">
+        <van-uploader class="uploader" v-model="detailInfo.fileList" multiple :after-read="afterRead"
+                      preview-size="26vw"/>
+      </div>
+    </van-cell-group>
     <div class="button">
-      <van-button type="primary" @click="commit" block round size="small">提交</van-button>
+      <van-button type="primary" @click="commit" block round size="small">上传</van-button>
     </div>
+    <van-popup v-model:show="farmlandChooseForm.show" round position="bottom">
+      <van-picker :columns="farmlandChooseForm.options"
+                  @cancel="farmlandChooseForm.show = false" @confirm="onFarmlandChooseFinish"/>
+    </van-popup>
+    <van-popup v-model:show="dosageUnitChooseForm.show" round position="bottom">
+      <van-picker :columns="dosageUnitChooseForm.options"
+                  @cancel="dosageUnitChooseForm.show = false" @confirm="onDosageUnitChooseFinish"/>
+
+    </van-popup>
   </div>
 
 </template>
@@ -104,18 +120,13 @@ const commit = (value) => {
   font-size: 14px;
 }
 
-.uploadImage {
-  margin: 10px;
-  display: flex;
-  flex-direction:row;
-  .span {
-    display: flex;
-    align-items: center; /* 确保文字垂直居中 */
-    min-width: 70px;
-  }
+.upload-container {
+  padding: 0 10px;
 }
+
 .button {
   border-radius: 15px;
+  margin-top: 8px;
 }
 
 .form {
