313e78167864e39890aa4e8b26565f70d6780842
Jiang Liu <emailtojiang@gmail.com>
2024-12-21 19:21:11 +0800

    feat: 提供空字符串、空数组的校验方法

---
+++ src/assets/js/public/validator.js
@@ -0,0 +1,43 @@
+/**
+ * 判断字符串是否为空
+ * 如果是 undefined 或者 null 也会返回 true
+ * 如果不是字符串，会打印警告信息，并返回 true
+ * @param value         需要判断的值
+ * @param trim          是否去掉前后空格
+ * @returns {boolean}   返回是否为空
+ */
+export const isEmptyString = (value, trim = false) => {
+    if (value === undefined || value === null) {
+        return true;
+    }
+    if (typeof value === 'string') {
+        if (trim) {
+            return value.trim() === '';
+        } else {
+            return value === '';
+        }
+    }
+    return true;
+}
+
+/**
+ * 是否为空数组，如果是 undefined 或者 null 也会返回 true
+ * 如果不是数组，会打印警告信息，并返回 true
+ * @param value         需要判断的值
+ * @returns {boolean}   返回是否为空数组
+ */
+export const isEmptyArray = (value) => {
+    if (value === undefined || value === null) {
+        return true;
+    }
+    if (value instanceof Array) {
+        return value.length === 0;
+    }
+    console.warn('需要判断的值不是数组：', value);
+    return true;
+}
+
+export default {
+    isEmptyString,
+    isEmptyArray
+}
