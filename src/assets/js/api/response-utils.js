066a3bcbd7d53bbcef853dcc6d684890bc4338fd
Jiang Liu <emailtojiang@gmail.com>
2024-12-21 19:19:45 +0800

    feat: 提供分页查询校验

--- src/assets/js/api/response-utils.js
+++ src/assets/js/api/response-utils.js
@@ -1,8 +1,38 @@
 /**
+ * 判断是否满足基本要求，非空，非undefined
+ * @param obj
+ * @returns {boolean}
+ */
+const basicValid = (obj) => {
+    return obj !== null && obj !== undefined;
+}
+
+/**
  * 判断请求是否成功
  * @param res {Result}  请求结果
  * @returns {boolean}   是否成功
  */
 export const isSuccessResponse = (res) => {
-    return Boolean(res?.code && res.code === 1);
+    return Boolean(
+        basicValid(res) &&
+        res?.code &&
+        res.code === 1
+    );
 };
+
+/**
+ * 判断是否是分页查询的结果，校验字段
+ * @param res {Page}    请求分页的结果
+ * @returns {boolean}   是否是分页查询的结果
+ */
+export const isSuccessPageResponse = (res) => {
+    return (
+        basicValid(res) &&
+        typeof res === 'object' &&
+        typeof res.current === 'number' &&
+        typeof res.pages === 'number' &&
+        Array.isArray(res.records) &&
+        typeof res.size === 'number' &&
+        typeof res.total === 'number'
+    );
+}
