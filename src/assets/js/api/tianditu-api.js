c2ef85a2b3a74e8ae68f0c78bcfd87aa51b5a7bc
Jiang Liu <emailtojiang@gmail.com>
2024-12-21 11:32:16 +0800

    修改API封装文件，调整天地图请求代码

--- src/assets/js/api/tianditu-api.js
+++ src/assets/js/api/tianditu-api.js
@@ -1,28 +1,25 @@
-import {ApiBuilder} from "@/assets/js/api/basic.js";
+import {createAxiosInstance} from "@/assets/js/api/axios-wrapper.js";
 import {tianDiTuToken} from "@/assets/js/secret/index.js";
 
 // 别骂了，官网 天地图 英文翻译就是 tianditu
-const tianDiTuApi = new ApiBuilder('http://api.tianditu.gov.cn')
+const tianDiTuApi = createAxiosInstance({
+    baseURL: 'http://api.tianditu.gov.cn',
+    timeout: 20000,
+    useToken: false,
+})
 
-const response_on_error = (err) => {
-    console.log('请求失败，请检查网络连接或联系管理员。错误原因：' + err.message);
-}
-
-export const getLocationText = async (latitude, longitude) => {
-    try {
-        const postStr = {
-            lon: longitude,
-            lat: latitude,
-            ver: 1
-        }
-        const res = await tianDiTuApi.get('/geocoder', {
+export const getLocationText = (latitude, longitude) => {
+    const postStr = {
+        lon: longitude,
+        lat: latitude,
+        ver: 1
+    }
+    return tianDiTuApi.get({
+        url: '/geocoder',
+        params: {
             postStr: JSON.stringify(postStr),
             type: 'geocode',
             tk: tianDiTuToken,
-        })
-        return res.data;
-    } catch (error) {
-        response_on_error(error);
-        throw error;
-    }
+        }
+    });
 }
