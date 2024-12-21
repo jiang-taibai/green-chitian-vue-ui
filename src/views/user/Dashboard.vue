83e7a0047249046486e50e5d589d3d16c5fbcc41
Jiang Liu <emailtojiang@gmail.com>
2024-12-21 15:47:04 +0800

    fix: 修复 Template 下有多个子节点的问题

--- src/views/user/Dashboard.vue
+++ src/views/user/Dashboard.vue
@@ -9,40 +9,42 @@ import NavBar from "@/components/public/NavBar.vue";
 </script>
 
 <template>
-  <nav-bar title="绿色赤田"/>
-  <div class="container">
-    <div class="toolbox-container">
-      <div class="cart-item">
-        <div class="title">
-          <van-icon class="title-icon" name="shortcut" class-prefix="iconfont" size="18" color="#EE0A24"/>
-          <span class="title-text">快捷入口</span>
-        </div>
-      </div>
-      <div class="content">
-        <quick-box/>
-      </div>
-    </div>
-    <div class="chart-container">
-      <div class="cart-item">
-        <div class="title">
-          <van-icon class="title-icon" name="chart" class-prefix="iconfont" size="18" color="#1989FA"/>
-          <span class="title-text">总化肥农药统计</span>
+  <div>
+    <nav-bar title="绿色赤田"/>
+    <div class="container">
+      <div class="toolbox-container">
+        <div class="cart-item">
+          <div class="title">
+            <van-icon class="title-icon" name="shortcut" class-prefix="iconfont" size="18" color="#EE0A24"/>
+            <span class="title-text">快捷入口</span>
+          </div>
         </div>
         <div class="content">
-          <dashboard-total-agrochemical-statistics/>
+          <quick-box/>
         </div>
       </div>
-      <div class="cart-item">
-        <div class="title">
-          <van-icon class="title-icon" name="chart" class-prefix="iconfont" size="18" color="#1989FA"/>
-          <span class="title-text">各化肥农药统计</span>
+      <div class="chart-container">
+        <div class="cart-item">
+          <div class="title">
+            <van-icon class="title-icon" name="chart" class-prefix="iconfont" size="18" color="#1989FA"/>
+            <span class="title-text">总化肥农药统计</span>
+          </div>
+          <div class="content">
+            <dashboard-total-agrochemical-statistics/>
+          </div>
         </div>
-        <div class="content">
-          <dashboard-various-agrochemical-statistics/>
+        <div class="cart-item">
+          <div class="title">
+            <van-icon class="title-icon" name="chart" class-prefix="iconfont" size="18" color="#1989FA"/>
+            <span class="title-text">各化肥农药统计</span>
+          </div>
+          <div class="content">
+            <dashboard-various-agrochemical-statistics/>
+          </div>
         </div>
       </div>
+      <user-tab-bar active="dashboard"/>
     </div>
-    <user-tab-bar active="dashboard"/>
   </div>
 </template>
 
