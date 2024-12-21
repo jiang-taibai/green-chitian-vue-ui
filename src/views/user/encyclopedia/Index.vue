b5ce6964260a5cc3dc90fb4a80f63a54d83433c6
Jiang Liu <emailtojiang@gmail.com>
2024-12-21 11:31:34 +0800

    完善百科指南界面，使用 Markdown 预览组件显示详细的指南

--- src/views/user/encyclopedia/Index.vue
+++ src/views/user/encyclopedia/Index.vue
@@ -1,28 +1,23 @@
 <script setup>
 import {ref} from 'vue';
 import NavBar from "@/components/public/NavBar.vue";
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+import {content} from "@/assets/md/encyclopedia.js";
 
-const searchKeyword = ref('');
+const text = ref(content);
 
-const onSearch = (value) => {
-  console.log(value);
-};
+VMdPreview.use(githubTheme, {});
 </script>
 
 <template>
   <div>
-    <nav-bar title="百科指南"/>
-    <van-search v-model="searchKeyword" show-action placeholder="请输入搜索关键词" @search="onSearch">
-      <template #action>
-        <div @click="onSearch">搜索</div>
-      </template>
-    </van-search>
+    <nav-bar title="指南"/>
     <div class="container">
       <div class="card">
-        <p>原功能需求不明确，待开发</p>
-        <p>功能需求：</p>
-        <p>1. 查询功能，能够查询推荐施肥量、病虫害绿色防控技术模式等</p>
-        <p>2. 后台管理员能够更新推荐施肥量、病虫害绿色防控技术模式这些信息</p>
+        <v-md-preview :text="text"></v-md-preview>
       </div>
     </div>
   </div>
@@ -31,9 +26,12 @@ const onSearch = (value) => {
 <style scoped lang="less">
 .container {
   padding: 16px;
+  min-height: 100vh;
+  background: #F2F2F2;
 }
+
 .card {
-  padding: 16px;
+  padding: 16px 0;
   background-color: #fff;
   border-radius: 8px;
 
