90584fa767857232c1d1bfa50bcfa9bff896d421
Jiang Liu <emailtojiang@gmail.com>
2024-12-21 19:20:12 +0800

    feat: 提供 Page、Field、FertilizationRecord 类的定义

--- src/assets/js/public/types.js
+++ src/assets/js/public/types.js
@@ -1,6 +1,46 @@
 /**
- * @typedef {Object} Result
+ * @typedef {Object} Result     请求结果
  * @property {Number} code      编码：1成功，0和其它数字为失败
  * @property {String} message   信息
  * @property {Object} data      数据
+ */
+
+/**
+ * @template T
+ * @typedef {Object} Page           请求分页结果
+ * @property {Number} current       当前页
+ * @property {Number} pages         总页数
+ * @property {Array.<T>} records    记录数组
+ * @property {Number} size          每页大小
+ * @property {Number} total         总记录数
+ */
+
+/**
+ * @typedef {Object} Field          农田
+ * @property {Number} id            农田 ID
+ * @property {String} fieldClass    农田类型
+ * @property {String} committee     所属村委会
+ */
+
+/**
+ * @typedef {Object} FertilizationRecord  施肥记录
+ * @property {Number} id                记录 ID
+ * @property {Number} fertilizerId      化肥 ID
+ * @property {String} applicationDate   施肥日期
+ * @property {Number} quantityUsed      使用的化肥数量
+ * @property {String} unit              使用量的计量单位
+ * @property {String} cropType          作物类型
+ * @property {Number} fieldId           地块 ID
+ * @property {Number} userId            用户 ID
+ * @property {String} username          用户名
+ * @property {String} method            施肥方法
+ * @property {String} weatherConditions  天气状况
+ * @property {String} soilMoisture      土壤湿度
+ * @property {String} notes             备注信息
+ * @property {Array} imageIds           图片 ID 数组
+ * @property {String} location          位置
+ * @property {String} createTime        记录创建时间
+ * @property {String} updateTime        记录更新时间
+ * @property {String} feildName         田地字段名称
+ * @property {String} fertilizerName    肥料名称
  */
