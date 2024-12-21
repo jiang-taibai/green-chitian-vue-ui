bb4e444b45bb1e555246442a47e2ef5b98a4afc5
Jiang Liu <emailtojiang@gmail.com>
2024-12-21 11:33:51 +0800

    新增 v-md-editor、pinia 组件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import {createApp} from 'vue'
-import './assets/css/style.css'
 import App from './App.vue'
-import router from './assets/js/router'
 import '@/assets/iconfont/iconfont.css'
+import './assets/css/style.css'
+import 'vant/lib/index.css';
+
+import router from './assets/js/router'
 import TMap from '@map-component/vue-tmap';
+import {createPinia} from 'pinia'
 
 const app = createApp(App)
+
+// 引入路由
 app.use(router)
+
+// 引入地图组件
 app.use(TMap)
+
+// 引入 Pinia
+app.use(createPinia())
 
 app.mount('#app')
