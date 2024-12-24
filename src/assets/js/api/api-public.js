ec0511971d9d5739c33881333ea23b64990af2a1
liujiang <emailtojiang@gmail.com>
2024-12-24 11:25:45 +0800

    feat: 添加公共API，获取所有的居委会、地块类型

---
+++ src/assets/js/api/api-public.js
@@ -0,0 +1,22 @@
+import {createAxiosInstance} from "@/assets/js/api/axios-wrapper.js";
+import {API_URL} from "@/assets/js/api/config.js";
+
+const api = createAxiosInstance({
+    baseURL: API_URL,
+    timeout: 5000,
+    useToken: true,
+});
+
+/**
+ * 查询所有的居委会，data 为 String[]
+ */
+export const getAllCommittee = () => {
+    return api.get('/field/block/fertilizer/committee/all');
+}
+
+/**
+ * 查询所有的地块类型
+ */
+export const getAllFieldClass = () => {
+    return api.get('/field/block/fertilizer/get/fieldclass/all', {});
+}
