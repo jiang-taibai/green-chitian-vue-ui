f0d8e7eba7732b9153839fc7eb87a034c699a962
liujiang <emailtojiang@gmail.com>
2024-12-30 15:45:32 +0800

    feat: 可以查询所有人的施肥记录，后面需要调整

--- src/assets/js/api/api-fertilizer.js
+++ src/assets/js/api/api-fertilizer.js
@@ -9,11 +9,12 @@ const api = createAxiosInstance({
 
 /**
  * 通过多条件查询（田地类型、委员会）查询所有地块的土壤养分信息
+ * TODO: 后续需要调整为只能访问个人的
  * @param fieldClass    田地类型
  * @param committee     委员会
  */
 export const queryFieldBlockFertilizerByMultiCondition = ({fieldClass, committee}) => {
-    return api.get('/field/block/fertilizer/user/list', {
+    return api.get('/field/block/fertilizer/all/list', {
         params: {
             fieldClass, committee
         }
