4895de22137b65f702da33fbcfe4bbd0d56da608
liujiang <emailtojiang@gmail.com>
2024-12-31 09:47:24 +0800

    style: 删除无用的代码

--- src/assets/js/public/system.js
+++ src/assets/js/public/system.js
@@ -8,8 +8,7 @@ const SYSTEM_CONFIG = {
     // 是否开启忘记密码服务
     FORGOT_PASSWORD_ENABLED: false,
     // 网站的基本URL，用于拼接回调 URL 等功能
-    // WEBSITE_BASE_URL: import.meta.env.PROD ? 'https://project.coderjiang.com/green-chitian' : 'http://localhost:5173',
-    WEBSITE_BASE_URL: 'https://project.coderjiang.com/green-chitian',
+    WEBSITE_BASE_URL: import.meta.env.PROD ? 'https://project.coderjiang.com/green-chitian' : 'http://localhost:5173',
 }
 
 export {
