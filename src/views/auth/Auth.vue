193ee5e57e808306fed4ceb654461b6bac004693
liujiang <emailtojiang@gmail.com>
2025-01-03 16:37:45 +0800

    feat: 对接微信登录

--- src/views/auth/Auth.vue
+++ src/views/auth/Auth.vue
@@ -6,6 +6,8 @@ import RegisterForm from "@/components/auth/RegisterForm.vue";
 import ForgetForm from "@/components/auth/ForgetForm.vue";
 import {useRouter} from 'vue-router';
 import {useUserStore} from "@/assets/js/store/user-info.js";
+import {wxLogin} from "@/assets/js/api/api-auth.js";
+import {isSuccessResponse} from "@/assets/js/api/response-utils.js";
 
 const router = useRouter();
 const userStore = useUserStore();
@@ -39,19 +41,18 @@ const tryLoginIfByWechat = () => {
   if (state !== 'wxlogin') {
     return
   }
-  showFailToast('微信登录正在调试中...')
-  // wxLogin(code).then(/** @param res {Result<WXLoginResponseData>} */res => {
-  //   if (isSuccessResponse(res)) {
-  //     userStore.setToken(res.data.jwt);
-  //     userStore.setNeedBindPhone(res.data.needBind)
-  //     showSuccessToast('登陆成功');
-  //     router.push({name: 'UserDashboard'});
-  //   } else {
-  //     showFailToast(res.message);
-  //   }
-  // }).catch(err => {
-  //   showFailToast(err.message);
-  // });
+  wxLogin(code).then(/** @param res {Result<WXLoginResponseData>} */res => {
+    if (isSuccessResponse(res)) {
+      userStore.setToken(res.data.jwt);
+      userStore.setNeedBindPhone(res.data.needBind)
+      showSuccessToast('登陆成功');
+      router.push({name: 'UserDashboard'});
+    } else {
+      showFailToast(res.message);
+    }
+  }).catch(err => {
+    showFailToast(err.message);
+  });
 }
 
 /**
