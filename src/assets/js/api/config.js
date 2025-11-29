7b3286ea6d73fd6e2630addb55a26e2989f6e9e2
Jiang Liu <emailtojiang@gmail.com>
2025-11-30 01:37:01 +0800

    feat: 切换后端测试API地址

--- src/assets/js/api/config.js
+++ src/assets/js/api/config.js
@@ -1,4 +1,4 @@
-const DEV_API_URL = 'http://106.53.88.197:10088';
+const DEV_API_URL = 'http://111.230.80.160:18088/';
 const PROD_API_URL = 'https://project-api.coderjiang.com/green-chitian/java-api';
 
 export const API_URL = import.meta.env.PROD ? PROD_API_URL : DEV_API_URL;
