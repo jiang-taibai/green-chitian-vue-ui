2feb1352e0c507f39835c0db782b024ef16ba4a6
CoderJiang <emailtojiang@gmail.com>
2025-12-13 12:28:56 +0800

    feat: 修改开发环境的 API 地址

--- src/assets/js/api/config.js
+++ src/assets/js/api/config.js
@@ -1,6 +1,6 @@
 import {getConfig} from "@/assets/js/public/config-center.js";
 
-const DEV_API_URL = 'http://121.4.117.66:8088';
+const DEV_API_URL = 'http://121.4.117.66:38088';
 const PROD_API_URL = getConfig().apiBaseUrl;
 
 export const API_URL = import.meta.env.PROD ? PROD_API_URL : DEV_API_URL;
