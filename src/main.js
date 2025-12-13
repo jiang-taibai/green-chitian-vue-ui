fda4dd573b9125a470b45ef5f054e35143dbdfb1
CoderJiang <emailtojiang@gmail.com>
2025-12-13 12:28:05 +0800

    feat: 格式化

--- src/main.js
+++ src/main.js
@@ -6,21 +6,21 @@ import './assets/css/style.css'
 import router from './assets/js/router'
 import TMap from '@map-component/vue-tmap';
 import {createPinia} from 'pinia'
-import { loadConfig } from '@/assets/js/public/config-center.js'
+import {loadConfig} from '@/assets/js/public/config-center.js'
 
 (async () => {
-  await loadConfig();
+    await loadConfig();
 
-  const app = createApp(App)
+    const app = createApp(App)
 
-  // 引入路由
-  app.use(router)
+    // 引入路由
+    app.use(router)
 
-  // 引入地图组件
-  app.use(TMap)
+    // 引入地图组件
+    app.use(TMap)
 
-  // 引入 Pinia
-  app.use(createPinia())
+    // 引入 Pinia
+    app.use(createPinia())
 
-  app.mount('#app')
+    app.mount('#app')
 })();
